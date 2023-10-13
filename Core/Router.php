337c068ba3914031b6233eedfd70399935b4ddc3
victor braz <victor.90braz@gmail.com>
2023-10-13 19:08:35 +0200

    clean up

--- Core/Router.php
+++ Core/Router.php
@@ -1,10 +1,9 @@
 <?php
-class Router {
-  private static $BASE_PATH = "C:\\Users\\braz9\\Desktop\\projects\\laracasts\\booking\\";
+  class Router {
+    private static $BASE_PATH = "C:\\Users\\braz9\\Desktop\\projects\\laracasts\\booking\\";
 
-  public static function basePath($route) {
-    return require_once self::$BASE_PATH . $route;
+    public static function basePath($route) {
+      return require_once self::$BASE_PATH . $route;
+    }
   }
-}
-
 ?>
