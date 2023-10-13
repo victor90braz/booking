6ad809c48c0ae12a13d48160ff12cd7b72ef4fe2
victor braz <victor.90braz@gmail.com>
2023-10-13 19:00:44 +0200

    create router

--- Core/Router.php
+++ Core/Router.php
@@ -1,9 +1,11 @@
 <?php
-class Router {
-  public $BASE_PATH = "C:\\Users\\braz9\\Desktop\\projects\\laracasts\\booking\\";
 
-  public function route($route) {
-    return require_once $this->BASE_PATH . $route;
+class Router {
+  private static $BASE_PATH = "C:\\Users\\braz9\\Desktop\\projects\\laracasts\\booking\\";
+
+  public static function route($route) {
+    return require_once self::$BASE_PATH . $route;
   }
 }
+
 ?>
--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 require_once '../Core/Router.php';
 
-$route = new Router();
-$route->route("Http/controllers/index.php");
+Router::route("Http/controllers/index.php");
+
 ?>
