0347416131bbd80844aa0b23d4a7a2ac67139f5e
victor braz <victor.90braz@gmail.com>
2023-10-15 21:44:23 +0200

    create a notes controller

--- Http/controllers/index.php
+++ Http/controllers/index.php
@@ -1,32 +1,3 @@
 <?php
-use Core\Database;
 
-require_once '../Core/functions.php';
-require_once '../Core/Database/Database.php';
-$config = require_once basePath('/config.php');
-
-class Notes {
-  private $config;
-
-  public function __construct($config) {
-      $this->config = $config;
-  }
-
-  public function title($text) {
-      return $text;
-  }
-
-  public function fetch() {
-      return (new Database($this->config['database']))->query('SELECT * FROM notes');
-  }
-}
-
-
-$notesManager = new Notes($config);
-
-$title = $notesManager->title("Booking Flats");
-$notes = $notesManager->fetch();
-
-require_once basePath('/views/index.view.php');
-?>
-
+require_once basePath("/Http/controllers/notes.php");
